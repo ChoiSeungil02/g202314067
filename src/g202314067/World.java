3aa64b73cb37ef28caefe1b42eb7ae5417e3ef8a
Choi, Seungil <jschoi020102@gmail.com>
2025-05-21 09:23:28 +0900

    five

--- src/g202314067/World.java
+++ src/g202314067/World.java
@@ -4,4 +4,9 @@ public class World {
 	  public static void run() {
 	    System.out.println("202314067");
 	  }
+	  
+	  public static void run2() {
+		    System.out.println("hello world");
+		  }
+
 	}
